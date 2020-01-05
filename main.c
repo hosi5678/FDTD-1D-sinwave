b7af34b94d73b921e326c4028afd7acb86f51dad
hosi5678 <hosi5678@gmail.com>
2020-01-06 07:08:36 +0900

    2nd commit 1) change precision(精度の変更) 2) delete useless array(不要な配列の削除)

--- main.c
+++ main.c
@@ -1,49 +1,83 @@
 #include "calc_param.h"
 #include "set_calc_param.h"
 #include "vec2D_Alloc.h"
-#include "setGaussian.h"
+#include "setgaussian.h"
 #include "setsinwave.h"
 #include "get1DEHVec.h"
+#include "put_memo.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 int main(void){
 
-	double *Ex,*Ey,*Ez;
-	double *Hx,*Hy,*Hz;
+	double *Ey;
+	double *Hz;
 
-	double *J; // J=I0*exp(-alpha(t-t0)^2);
+	double *J; 
+
+	clock_t start,end;
+
+	start=clock();
 
 	// set calculation point
 	set_calc_param();
 
 	// 1D_Alloc();
-	Ex=calloc(Ex_length,sizeof(double));
-	Ey=calloc(Ey_length,sizeof(double));
-	Ez=calloc(Ez_length,sizeof(double));
+	Ey=calloc((unsigned int)Ey_length,sizeof(double));
+	Hz=calloc((unsigned int)Hz_length,sizeof(double));
 
-	Hx=calloc(Hx_length,sizeof(double));
-	Hy=calloc(Hy_length,sizeof(double));
-	Hz=calloc(Hz_length,sizeof(double));
+	J=calloc((unsigned int)timestep,sizeof(double));
 
-	J=calloc(timestep,sizeof(double));
+	if(NULL==Ey){
+		printf("Ey memory allocation error.\n");
+		exit(1);
+	}
 
-//	setGaussian(J);
-	setsinwave(J);
+	if(NULL==Hz){
+		printf("Hz memory allocation error.\n");
+		exit(1);
+	}
 
-	Ey=get1DEHVec(Ey,Hz,J);
+	if(NULL==J){
+		printf("J memory allocation error.\n");
+		exit(1);
+	}
 
-	free(Ex);
+	if(wave_selection==0){
+		setgaussian(J);
+	}else if(wave_selection==1){
+		setsinwave(J);
+	}else{
+		printf("select wave value error.(0 or 1).\n");
+		exit(1);
+	}
+
+   get1DEHVec(Ey,Hz,J);
+
+	FILE *fp;
+
+	fp=fopen("memo.csv","w");
+
+	if(fp==NULL){
+		printf("file open error.\n");
+		exit(1);
+	}
+
+	fp=put_memo(fp,"memo.csv");
+
+	fclose(fp);
+
 	free(Ey);
-	free(Ez);
-
-	free(Hx);
-	free(Hy);
 	free(Hz);
 
 	free(J);
+
+	end=clock();
+
+	printf("required time:%.2f(sec)\n",(double)(end-start)/CLOCKS_PER_SEC);
 
 	return 0;
 }
